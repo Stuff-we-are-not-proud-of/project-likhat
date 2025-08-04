c1eb2d5640679715d0a2c28bca2d80e150a1b29c
Krishna Gera <krishnagera93@gmail.com>
2025-08-04 12:23:14 +0530

    Issue in single step

--- Coding/max_pooling_krishna.py
+++ Coding/max_pooling_krishna.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def max_pool(image_matrix, size, stride):
+def max_pool(image_matrix, size=(2,2), stride=2):
     input_sizes = image_matrix.shape
-    output_sizes = ((input_sizes[0] - size) // stride + 1, (input_sizes[1] - size) // stride + 1, input_sizes[2])
+    output_sizes = ((input_sizes[0] - size[0]) // stride + 1, (input_sizes[1] - size[1]) // stride + 1, input_sizes[2])
     output_matrix = np.zeros(shape=output_sizes)
 
     for i in range(output_sizes[2]):
@@ -11,6 +11,6 @@ def max_pool(image_matrix, size, stride):
                 row_starter = i*stride
                 column_starter = j*stride
 
-                output_matrix[j,k,i] = np.max(image_matrix[row_starter:row_starter+size, column_starter:column_starter+size, i])
+                output_matrix[j,k,i] = np.max(image_matrix[row_starter:row_starter+size[0], column_starter:column_starter+size[1], i])
     
     return output_matrix
--- Coding/single_convolution_krishna.py
+++ Coding/single_convolution_krishna.py
@@ -1,29 +1,49 @@
 import numpy as np
 from ppm_to_matrix import ppm_to_matrix 
 
-def generate_filters(filter_size, input_channels=3, num_filters=32):
+def generate_filters(filter_size, input_channels, num_filters=32):
     fan_in = filter_size[0] * filter_size[1] * input_channels
     limit = np.sqrt(6 / fan_in)
     filters = np.random.uniform(-limit, limit, size=(num_filters, filter_size[0], filter_size[1], input_channels))
     biases = np.zeros(num_filters)
     return filters, biases
 
-def padding_image(image_matrix, pad=1):
+def padding_image(image_matrix, pad):
     return np.pad(image_matrix, ((pad, pad), (pad, pad), (0, 0)), mode='constant', constant_values=0)
 
-def single_convolution_layer(image_path, filter_size=(3, 3), stride=1):
+def single_convolution_layer(image_path, filter_size=(3, 3), stride=1, num_filters=32, padding=1):
     image_matrix = ppm_to_matrix(image_path)
-    filters, biases = generate_filters(filter_size)
-    padded_image = padding_image(image_matrix, pad=(filter_size[0] // 2))
-    input_height, input_width, input_channels = padded_image.shape
-    output_height = ((input_height - filter_size[0]) // stride) + 1
-    output_width = ((input_width - filter_size[1]) // stride) + 1
-    num_filters = filters.shape[0]
+    input_channels = image_matrix.shape[2]
+    filters, biases = generate_filters(filter_size, input_channels, num_filters)
+    padded_image = padding_image(image_matrix, pad=padding)
+    input_height, input_width, _ = image_matrix.shape  # Unpadded size for output calculation
+    output_height = ((input_height + 2*padding - filter_size[0]) // stride) + 1
+    output_width = ((input_width + 2*padding - filter_size[1]) // stride) + 1
     output_matrix = np.zeros((output_height, output_width, num_filters))
     
-    for i in range(0, output_height, stride):
-        for j in range(0, output_width, stride):
-            subarray = padded_image[i:i+filter_size[0], j:j+filter_size[1], :]
+    for i in range(output_height):
+        for j in range(output_width):
+            subarray = padded_image[i*stride:i*stride + filter_size[0], j*stride:j*stride + filter_size[1], :]
+            broadcasted_subarray = subarray[np.newaxis, :, :, :]
+            multiplied = filters * broadcasted_subarray
+            conv_sum = np.sum(multiplied, axis=(1,2,3))
+            output = np.maximum(0, conv_sum + biases)
+            output_matrix[i, j, :] = output
+    
+    return output_matrix 
+
+def single_convolution_layer_from_matrix(image_matrix, filter_size=(3, 3), stride=1, num_filters=32, padding=1):
+    input_channels = image_matrix.shape[2]
+    filters, biases = generate_filters(filter_size, input_channels, num_filters)
+    padded_image = padding_image(image_matrix, pad=padding)
+    input_height, input_width, _ = image_matrix.shape  # Unpadded
+    output_height = ((input_height + 2*padding - filter_size[0]) // stride) + 1
+    output_width = ((input_width + 2*padding - filter_size[1]) // stride) + 1
+    output_matrix = np.zeros((output_height, output_width, num_filters))
+    
+    for i in range(output_height):
+        for j in range(output_width):
+            subarray = padded_image[i*stride:i*stride + filter_size[0], j*stride:j*stride + filter_size[1], :]
             broadcasted_subarray = subarray[np.newaxis, :, :, :]
             multiplied = filters * broadcasted_subarray
             conv_sum = np.sum(multiplied, axis=(1,2,3))
